ff31d970c39735afc88814cf88e788856a57f6b3
Simen <simen_aabol@hotmail.com>
2022-02-08 19:44:10 +0100

    yuiopå

--- Project1/actor_critic/sim_world.py
+++ Project1/actor_critic/sim_world.py
@@ -1,19 +1,9 @@
-import sys # added!
-sys.path.append("..") # added!
-
-
-from Project1.sim_worlds.gambler import Gambler
-from Project1.sim_worlds.cart import Cart
-from Project1.sim_worlds.hanoi import Hanoi
-
-
-
-
-from Project1.parameters import config, cartConfig, gamblerConfig, hanoiConfig
-
+from sim_worlds.gambler import Gambler
+from sim_worlds.cart import Cart
+from sim_worlds.hanoi import Hanoi
 
 class Sim_world():
-    def __init__(self):
+    def __init__(self, config):
 
         """ Set initial game configs here """
 
@@ -22,14 +12,14 @@ class Sim_world():
         elif config["problem"] == "gambler":
             self.problem = Gambler()
         elif config["problem"] == "hanoi":
-            self.problem = Hanoi(hanoiConfig["game_config"]['pegs'], hanoiConfig["game_config"]['discs'])
+            self.problem = Hanoi()
         else:
             raise Exception('Sim_world must be cart, gambler, or hanoi.')
 
         self.config = config
 
     def get_initial_game_state(self):
-
+    
         """ Create game object from config and get states """
         return self.problem.get_state(), self.problem.game_over(), self.problem.get_legal_moves()
 
