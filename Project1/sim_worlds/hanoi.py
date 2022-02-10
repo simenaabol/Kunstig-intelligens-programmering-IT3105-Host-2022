6fdbfe3861c8c1b6697db21ad417082e8cf9240e
MarcusDragsten <marcuskd@hotmail.com>
2022-02-10 12:27:38 +0100

    gotten further on actor and hawaii/gambler

--- Project1/sim_worlds/hanoi.py
+++ Project1/sim_worlds/hanoi.py
@@ -7,83 +7,79 @@ import numpy as np
 class Hanoi():
     def __init__(self, pegs, discs) :   
 
+        self.number_of_pegs = pegs
+        self.number_of_discs = discs
 
-        self.nPegs = pegs
-        self.nDiscs = discs
-
-        lPegs = []   # List that rep. all the pegs with the discs. Higer int = bigger discs 
+        pegs_list = []   # List that rep. all the pegs with the discs. Higer int = bigger discs 
         for i in range(pegs): # create a list with nested lists
-            lPegs.append([])
+            pegs_list.append([])
             for n in range(discs):
                 if i == 0:
-                    lPegs[i].append(discs-n) # Place the discs in the first peg
+                    pegs_list[i].append(discs-n) # Place the discs in the first peg
                 #else:
-                #   lPegs[i].append(0) # fills nested lists with data
-        self.lPegs = lPegs
+                #   pegs_list[i].append(0) # fills nested lists with data
+        self.pegs_list = pegs_list
 
     def get_state(self):
-        return self.lPegs
+        return self.pegs_list
 
     def get_state_key(self):
-        # print(tuple(map(tuple, self.lPegs)))
-        return tuple(map(tuple, self.lPegs))
+        # print(tuple(map(tuple, self.pegs_list)))
+        return tuple(map(tuple, self.pegs_list))
 
     def get_legal_moves(self):
 
-        pegWithDisc = self.peg_with_disc()
-        # print('pegWithDisc:',  pegWithDisc)
+        peg_with_disc = self.peg_with_disc()
+        # print('peg_with_disc:',  peg_with_disc)
 
-        lMoves = []
-        for peg in pegWithDisc:
+        moves_list = []
+        for peg in peg_with_disc:
             temp_moves = self.get_legal_move_from_peg(peg)
             if len(temp_moves) != 0:
-                lMoves += temp_moves
-            # lMoves+=(self.get_legal_move_from_peg(peg))
-        # print('LMOVES',lMoves)
-        return lMoves
+                moves_list += temp_moves
+            # moves_list+=(self.get_legal_move_from_peg(peg))
+        # print('LMOVES',moves_list)
+        return moves_list
 
-    def get_legal_move_from_peg(self, pegWithDisc): #pegWithDisc is the peg which disc is from
+    def get_legal_move_from_peg(self, peg_with_disc): #peg_with_disc is the peg which disc is from
         move = []
-        lPegs = self.get_lPegs()
+        pegs_list = self.get_pegs_list()
 
-
-        #for i in range(len(pegWithDisc)+1): # i blir hvilken peg vi henter fra
-        hDisc = lPegs[pegWithDisc][-1]
-        # print('hDisc', hDisc)
-        for j, peg in enumerate(lPegs): # J er hvilken peg vi er i
+        #for i in range(len(peg_with_disc)+1): # i blir hvilken peg vi henter fra
+        highest_disc = pegs_list[peg_with_disc][-1]
+        # print('highest_disc', highest_disc)
+        for j, peg in enumerate(pegs_list): # J er hvilken peg vi er i
                 
                 if 0 == len(peg):
-                    move.append([pegWithDisc, j])
-                elif peg[-1] > hDisc:
-                    move.append([pegWithDisc, j]) # legger til hvilken peg man kan flytt fra, og til
+                    move.append([peg_with_disc, j])
+                elif peg[-1] > highest_disc:
+                    move.append([peg_with_disc, j]) # legger til hvilken peg man kan flytt fra, og til
                 else:
                     continue
                         
-                
-        
         return move
 
     def reset_game(self):
 
-        pegs = self.nPegs
-        discs = self.nDiscs
-        lPegs = self.lPegs
+        pegs = self.number_of_pegs
+        discs = self.number_of_discs
+        pegs_list = self.pegs_list
 
-        lPegs = []   # List that rep. all the pegs with the discs. Higer int = bigger discs 
+        pegs_list = []   # List that rep. all the pegs with the discs. Higer int = bigger discs 
         for i in range(pegs): # create a list with nested lists
-            lPegs.append([])
+            pegs_list.append([])
             for n in range(discs):
                 if i == 0:
-                    lPegs[i].append(discs-n) # Place the discs in the first peg
+                    pegs_list[i].append(discs-n) # Place the discs in the first peg
                 #else:
-                #   lPegs[i].append(0) # fills nested lists with data
-        self.lPegs = lPegs
+                #   pegs_list[i].append(0) # fills nested lists with data
+        self.pegs_list = pegs_list
 
     def peg_with_disc(self):
-        lPegs = self.get_lPegs()
+        pegs_list = self.get_pegs_list()
         pegs = []
 
-        for i, peg in enumerate(lPegs):
+        for i, peg in enumerate(pegs_list):
             if 0 != len(peg):
                 pegs.append(i)
 
@@ -92,78 +88,59 @@ class Hanoi():
 
 
     def find_highest_disc_in_peg(self):
-        lPegs = self.get_lPegs()
+        pegs_list = self.get_pegs_list()
         
         
  
         #Get the highest disc in thepeg [pegNumber]
-        hDisc = 0
-        for peg in lPegs:
+        highest_disc = 0
+        for peg in pegs_list:
             for disc in peg:
                 if 0 != len(peg):
 
                     # print(peg)
-                    hDisc = peg[-1]
+                    highest_disc = peg[-1]
 
                 else:
                     break
-        return hDisc
+        return highest_disc
             
                               
     def take_action(self, move):
         # print(move)
-        # print(self.lPegs)
-        lPegs = self.get_lPegs()
-        fromPeg = move[0]
-        toPeg = move[1]
-        # print(fromPeg, toPeg)
-        lPegs[toPeg].append(lPegs[fromPeg][-1]) # Adds the disc to the new peg
-        lPegs[fromPeg].pop()
+        # print(self.pegs_list)
+        pegs_list = self.get_pegs_list()
+        from_peg = move[0]
+        to_peg = move[1]
+        # print(from_peg, to_peg)
+        pegs_list[to_peg].append(pegs_list[from_peg][-1]) # Adds the disc to the new peg
+        pegs_list[from_peg].pop()
 
 
     def game_done(self):
-        lPegs = self.get_lPegs()
-        discs = self.get_nDiscs()
+        pegs_list = self.get_pegs_list()
+        discs = self.get_number_of_discs()
 
-        # print(self.lPegs)
-        
-        for i in range(len(lPegs)):
-            if i == 0:
-                # print('ut')
-                continue
-            else:
-                if discs+1 == len(lPegs[i]): 
-                # if (discs in lPegs[i]):
-                    # if discs == lPegs[i][1]:   
-                    # if len(lPegs[0]) == discs:
-                        # print('fuck',len(lPegs[0]), discs )
-                        
-                        return [1, True]
-                        
+        # print(self.pegs_list)
 
         return [0, False]
-    
 
+    def get_number_of_pegs(self):
+        return self.number_of_pegs
 
+    def get_number_of_discs(self):
+        return self.number_of_pegs
 
-            
+    def get_pegs_list(self):
+        return self.pegs_list
 
-    def get_nPegs(self):
-        return self.nPegs
-
-    def get_nDiscs(self):
-        return self.nPegs
-
-    def get_lPegs(self):
-        return self.lPegs
-
-    def get_aDiscs(self):
-        return self.aDiscs
+    # def get_aDiscs(self):
+    #     return self.aDiscs
     
     def get_graphic(self):
-        lPegs = self.get_lPegs()
-        dWith = self.get_nDiscs()
-        nPegs = self.get_nPegs()
+        pegs_list = self.get_pegs_list()
+        dWith = self.get_number_of_discs()
+        number_of_pegs = self.get_number_of_pegs()
         dWithG = dWith+3
 
         
@@ -171,7 +148,7 @@ class Hanoi():
         fig, ax = plt.subplots()
 
         #add rectangle to plot
-        for i, peg in enumerate(lPegs):
+        for i, peg in enumerate(pegs_list):
             ax.plot([(dWith*0.5)-0.5+i*(dWithG), (dWith*0.5)-0.5+i*(dWithG)], [0, dWith-0.5])
             
             for j,disc in enumerate(peg):
@@ -179,11 +156,9 @@ class Hanoi():
                     ax.add_patch(Rectangle((j*0.5+i*dWithG, j), disc, 1,  
                     edgecolor ="blue", linewidth=1, facecolor  ='yellow'))
               
-                
-
 
         #create simple line plot.
-        ax.plot([0, (dWith+2)*nPegs], [dWith, dWith], color = "white")
+        ax.plot([0, (dWith+2)*number_of_pegs], [dWith, dWith], color = "white")
 
         return plt.show()
 
@@ -193,17 +168,17 @@ class Hanoi():
 
 
 Game = Hanoi(3,3)#Pegs and discs
-''' print('Sate of the game', Game.get_lPegs())
+''' print('Sate of the game', Game.get_pegs_list())
 moves = Game.get_legal_moves()
 print('Legal moves', moves)
 Game.take_action([0,4])
-print('Sate of the game', Game.get_lPegs())
+print('Sate of the game', Game.get_pegs_list())
 moves = Game.get_legal_moves()
 print('Legal moves', moves)
 print(Game.game_done()) '''
 
 
-# print(Game.get_lPegs())
+# print(Game.get_pegs_list())
 
 # print(Game.get_legal_moves())
 # Game.get_graphic()
