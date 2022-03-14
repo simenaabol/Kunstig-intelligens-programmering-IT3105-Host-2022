c70e8b00cddfaeaac91a8c63b945d0ec2ae4d1a3
Simen <simen_aabol@hotmail.com>
2022-03-14 13:00:23 +0100

    fiksa litt og lagt til get_normalized_distribution

--- Project2/MCTS/MonteCarloTreeSearch.py
+++ Project2/MCTS/MonteCarloTreeSearch.py
@@ -1,6 +1,6 @@
 # import sys
 # sys.path.append("..") 
-
+ 
 from Node import Node
 import numpy as np
 
@@ -12,6 +12,15 @@ import numpy as np
 
 class MCTS:
     def ___init___(self, exploration_weight, actor, stateManager):
+        """
+
+        Class for Monte Carlo Searc Tree
+
+        PARAMS: exploration_weight, actor, stateManager
+
+        RETURN: nothing
+
+        """
         self.root = Node(
             state = stateManager.get_state(),
             parent = None,
@@ -22,8 +31,53 @@ class MCTS:
         self.actor = actor # obj
         self.stateManager = stateManager # obj
 
+# Main-kok
+    def get_normalized_distribution(self):
+        """
+
+        Method fetching the distribution
+
+        PARAMS: nothing
+
+        RETURNS: the normalized distribution 
+
+        """
+
+        
+        """
+        Gets the distribution of visit counts of the children of the root
+        :return: The normalized distribution
+        """
+        # Mix med niclas
+        board_shape = self.simworld.get_grid().shape
+        distribution = np.zeros(board_shape)
+        for action in self.root.kid: #Itererer alle barna til roten? Ikke alle?
+                distribution.append(float(self.root.kid[action].count) / float(self.root.count))
+        return distribution.flatten()
+
+        # Dette er fra main. Kan evt testes
+        # board_shape = self.simworld.get_grid().shape
+        # distribution = np.zeros(board_shape)
+        # for action in self.root.kid: #Itererer alle barna til roten? Ikke alle?
+        #     kid = self.root.kid[action]
+        #     distribution[action] = kid.count
+        # distribution = distribution.flatten()
+        # return distribution / np.sum(distribution)    
+
+        
+
+
 
     def mcts(self):
+        """
+
+        Method for running the mcts algorithm.
+
+        PARAMS: nothing 
+
+        RETURNS: nothing
+
+        """
 
         if self.stateManager.game_done(self.root.get_state): #Sjekk opp metodNavn til done
             return # Kunne returnert noe her for å skille seg fra kok. 
@@ -67,22 +121,18 @@ class MCTS:
         # Kommenatren under er kok
         # If only nodes in MCT should be updated change leaf to successor
         self.backpropagation(kid, rew)
-
-
-
-
-        
-
-
-
-
-
-
-
-
-    
+   
 
     def update_root(self, action):
+        """
+
+        Method for updating the root
+
+        PARAMS: action
+
+        RETURNS: nothing
+
+        """
         _temp_root = self.root.get_kid(action)
         if not _temp_root: # kok - usikker hva som skjer her
             self.root = Node(
@@ -93,6 +143,15 @@ class MCTS:
         self.root = _temp_root
 
     def search(self):
+        """
+
+        Method for traversing the tree from the root to a leaf node by using the actor.
+
+        PARAMS: nothing
+
+        RETURNS: a leaf node
+
+        """
 
 
         current_node = self.root
@@ -107,24 +166,65 @@ class MCTS:
         else:
             raise TypeError("Sorry, the player int is not compatible")
 
-        _best = -flag * float('inf')
-        _best_action = None
+        max = -flag * float('inf')
+        current_best = None
         kids = current_node.get_kids()
 
         for action in kids:
-            # hvrfor a?
+            # hvorfor a?
             a = current_node.get_q_value(action) + flag * current_node.get_u_value(action, self.exp_weight)
             
-            if (a < _best and flag == -1) or (a > _best and flag == 1):
-                _best = a
-                _best_action = action
+            if (a < max and flag == -1) or (a > max and flag == 1):
+                max = a
+                current_best = action
 
             flag *= -1
-            current_node = current_node.get_kid_with_action(_best_action)
+            current_node = current_node.get_kid_with_action(current_best)
             return current_node
 
 
+    
+    def new_leafs(self, leaf): # Node expansion
+        """
+
+        Method - Generating all(atm) child states of a parent state, 
+              and then connecting the tree node housing the parent state (a.k.a. parent node) 
+              to the nodes housing the kid states (a.k.a. kid nodes)
+
+        PARAMS: leaf-node
+
+        RETURNS: a set of new kids from the input parent 'leaf'
+
+        """
+        # merk at denne finne alle nye leafs. 
+
+        state = leaf.get_state()
+        # player = leaf.get_player()
+
+        state_action_list, player = self.stateManager.get_kids(state, player)
+        kids = []
+
+        for state, action in state_action_list:
+            kids = leaf.get_kid_with_action(action, rollout = True)
+            leaf.remove_kid(action, rollout = True)
+
+            if not kid: # Sjekk om man kan bruke noe annet her
+                kid = Node(state, leaf, player)
+            leaf.add_kid(kid, action)
+            kids.append(kid)
+        return kids
+
+
     def leaf_evaluation(self, from_node):  # rollout
+        """
+
+        Method for traversing the tree from the root to a leaf node by using the actor.
+
+        PARAMS: from_node
+
+        RETURNS: a leaf node
+
+        """
         
         leaf = from_node.get_state()
         player = from_node.get_player()
@@ -146,28 +246,22 @@ class MCTS:
         rew = self.stateManager.get_reward(final_leaf) # Kan smelle disse sammen
         # Disse to over bør egt ikke lagres som egne variabler, da de ikke brukes    
         return parent , rew
-    
-    def new_leafs(self, leaf): # Node expansion
-        # merk at denne finne alle nye leafs. Den originale MCST gjør ikke dette. 
 
-        state = leaf.get_state()
-        # player = leaf.get_player()
-
-        state_action_list, player = self.stateManager.get_kids(state, player)
-        kids = []
-
-        for state, action in state_action_list:
-            kids = leaf.get_kid_with_action(action, rollout = True)
-            leaf.remove_kid(action, rollout = True)
-
-            if not kid: # Sjekk om man kan bruke noe annet her
-                kid = Node(state, leaf, player)
-            leaf.add_kid(kid, action)
-            kids.append(kid)
-        return kids
 
     def backpropagation(self, leaf, rew):
+        """
 
+        Method - Passing the evaluation of a final state back up the tree,
+              updating relevant data at all nodes and edges on the path from 
+              the final state to the tree root.
+
+        PARAMS: leaf-node , reward
+
+
+        RETURNS: a set of new kids from the input parent 'leaf'
+
+        """
+    
         current_node = leaf
 
         while self.root != current_node:
@@ -179,6 +273,8 @@ class MCTS:
         
         self.root.update_evaluate(rew)
         self.root.update_count()
+
+
 
 
 
