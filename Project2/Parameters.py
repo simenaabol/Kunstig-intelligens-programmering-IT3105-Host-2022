b2d848a61cb85dc200e8582f40f457d150a9d663
Marcus Dragsten <marcuskd@hotmail.com>
2022-04-07 07:47:04 +0200

    nets for demo

--- Project2/Parameters.py
+++ Project2/Parameters.py
@@ -11,7 +11,7 @@ config = {
     
     'mode': 'learn', # learn, learn_topp, topp
     
-    'network_folder_name': 'natt_test2' # Folder name for saving and loading the networks for TOPP
+    'network_folder_name': '4_hex_500' # Folder name for saving and loading the networks for TOPP
 }
 
 hex_config = {
